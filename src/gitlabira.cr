ac322f41d9ab1e1b19021d38b59b02c3bd6ad1a7
Luu Nguyen <nnluukhtn@gmail.com>
2019-10-07 17:11:12 +0700

    Print request body

--- src/gitlabira.cr
+++ src/gitlabira.cr
@@ -1,6 +1,17 @@
-# TODO: Write documentation for `Gitlabira`
+# TODO: Write documentation for `Gitlabira`'
+require "kemal"
+require "logger"
+
 module Gitlabira
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  post "/hook" do |env|
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::INFO
+    logger.info env.request.body.not_nil!.gets_to_end
+
+    "OK"
+  end
 end
+
+Kemal.run
